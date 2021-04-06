2f77e3159d801cc6dcd5c1640feee2b323cb9c53
Yazan <yazann.abbas@gmail.com>
2021-04-06 10:44:30 +0200

    fixed search method

--- src/pages/results.jsx
+++ src/pages/results.jsx
@@ -2,9 +2,9 @@ import React, { useEffect, useState } from "react";
 import PackageDescription from "../components/packageDescription";
 
 export default function Results(props) {
-  //const queryParcelId = props.match.params.id
-  const pkg = props.data.filter((i) => i.id == props.match.params.id);
-  //i.parcel_id == queryParcelId
+  const queryParcelId = props.match.params.id;
+  const pkg = props.data.filter((i) => i.parcel_id == queryParcelId);
+
   console.log("pkg", pkg);
   return (
     <div>
